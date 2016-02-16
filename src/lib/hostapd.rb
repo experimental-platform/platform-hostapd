d6ecf71c68fcce9706e104d6e1dab3d79bf5b607
Hinnerk Haardt <haardt@information-control.de>
2016-02-16 09:21:59 +0100

    Test and fix hardcoded `phy0`.

--- src/lib/hostapd.rb
+++ src/lib/hostapd.rb
@@ -413,7 +413,12 @@ module Wifi
     end
 
     def ieee80211n
-      !!`iw phy phy0 info`.match(/HT[248]{1}0/im) ? "1" : "0"
+      if result = `iw list`.match(/(^[a-zA-Z0-9]+)\s+([a-zA-Z0-9]+)/)
+        phy = result[2]
+      else
+        phy = 'phy0'
+      end
+      !!`iw phy #{phy} info`.match(/HT[248]{1}0/im) ? "1" : "0"
     end
 
     def mac_address
@@ -709,7 +714,7 @@ except-interface=lo
   module Generator
     class HardwareCapability
       DEFAULT_OPTIONS = {
-        iw_info: 'iw %{interface} info',
+        iw_info: 'iw phy %{interface} info',
         iw_capabilities_match: /band 1\:[\S\s]+?capabilities\:(?<capabilities>[\S\s]+?)frequencies\:/i,
         channel: "1"
       }
