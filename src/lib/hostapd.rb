730feccc128a5187b4cd827ac892693347eab165
Hinnerk Haardt <haardt@information-control.de>
2016-03-08 14:02:08 +0100

    Apparently the hardware needs a few seconds.

--- src/lib/hostapd.rb
+++ src/lib/hostapd.rb
@@ -93,8 +93,10 @@ module Wifi
     raise "Renaming #{interface} to #{name} didn't work: #{output}" unless $?.success?
     output = `ip link set dev #{name} up 2>&1`
     raise "Starting #{name} didn't work: #{output}" unless $?.success?
+    sleep 5
     output = `systemctl restart systemd-networkd`
     raise "Restarting networkd didn't work: #{output}" unless $?.success?
+    sleep 5
   end
 
   Contract Array => Array
